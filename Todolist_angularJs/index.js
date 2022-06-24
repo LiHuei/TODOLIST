4a7881b85a80ac96027fa4ac8f40a5a0c91faf36
li_huei <hccpaps40832@gmail.com>
2022-06-24 17:48:03 +0800

    Update

--- Todolist_angularJs/index.js
+++ Todolist_angularJs/index.js
@@ -1,11 +1,20 @@
 angular.module('todoApp', [])
     .controller('todoController', function () {
         var todoList = this;
-        todoList.todos = [
-            { text: 'TODO 1', done: false, clickStar: false, date: '', time: '', memo: '', },
-            { text: 'TODO 2', done: false, clickStar: false, },
-            { text: 'TODO 3', done: false, clickStar: false, },
+        this.todos = [
+            new todo('TODO 1'),
+            new todo('TODO 2'),
+            new todo('TODO 3'),
         ]
+
+        this.tabs = [
+            new Tab('myTask'),
+            new Tab('inProgress'),
+            new Tab('completed'),
+        ]
+
+
+        // this.currentTab = 'all';
 
         todoList.addTodo = function () {
             var newTodo = { text: todoList.newTodoText, done: false, clickStar: false };
@@ -18,8 +27,62 @@ angular.module('todoApp', [])
             return left.length;
         }
 
-        todoList.inProgress = function () {
-            var inProgress = todoList.todos.filter(todo => todo.done == false);
-            console.log(inProgress);
+        // this.inProgress = function () {
+        //     this.inProgressTodos = todoList.todos.filter(todo => todo.done == false);
+        //     this.currentTab = 'inProgress';
+        // }
+
+        // this.toMyTasks = function () {
+        //     this.currentTab = 'myTask';
+        // }
+
+        // this.toCompleted = function () {
+        //     this.allTodos = todoList.todos.filter(todo => todo.done == true);
+        //     this.currentTab = 'completed';
+        // }
+
+        this.selectTab = function (tab) {
+            todoList.tabs.forEach(item => item.active = false)
+            tab.select();
+            this.currentTab = tab.text;
+
+            if (tab.isInProgress) {
+                this.filterTodos = todoList.todos.filter(todo => todo.done == false);
+            }
+            if (tab.text == 'completed') {
+                this.filterTodos = todoList.todos.filter(todo => todo.done == true);
+            }
+
         }
+
+        this.selectTab(this.tabs[0])
     })
+
+class todo {
+    text;
+    done = false;
+    clickStar = false;
+    constructor(text) {
+        this.text = text;
+    }
+
+    isShow(value) {
+        this.done = value;
+    }
+}
+
+class Tab {
+    text;
+    active;
+    constructor(text) {
+        this.text = text;
+    }
+
+    select() {
+        this.active = true;
+    }
+
+    get isInProgress() {
+        return this.text == 'inProgress'
+    }
+}
